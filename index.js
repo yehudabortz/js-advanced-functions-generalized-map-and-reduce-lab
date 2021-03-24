0b05b765f51f0d9b04a8d7e49f2ae2e25e06fbfa
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-24 15:34:48 -0500

    commit tree 7bd8af53b3f72b84cb84e6547dad22b04050745a

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your functions here
+
+function map(array, func) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(func(array[i]))
+    }
+    return newArray
+}
+
+function reduce(array, func, starting) {
+    let r
+    let i
+    if (!!starting) {
+        r = starting
+    } else {
+        r = array[0]
+    }
+
+    if (!!starting) {
+        i = 0
+    } else {
+        i = 1
+    }
+    for (; i < array.length; i++) {
+        r = func(array[i], r)
+    }
+    return r;
+}
+
+
+
